4d7bc52e432b506e7891659d6fbdb834efcd3097
guinnod <210103064@stu.sdu.edu.kz>
2023-06-12 17:40:42 +0600

    host

--- back/settings.py
+++ back/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o%gre7p#gn53-aqvhqwq=e1y#)#b6$13zp1rk)sl+uv7)u_!qw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
